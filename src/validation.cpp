3ffeb176ecd284d9020f0b53ece5ae4e22aaf57e
Ved9rakash <vs19990711@gmail.com>
2022-04-24 20:34:38 +0530

    Validation Layers

--- src/validation.cpp
+++ src/validation.cpp
@@ -22,11 +22,12 @@ bool HelloTriangleApplication::checkValidationSupport()
 	std::vector<VkLayerProperties> availableLayers(layerCount);
 	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
+	// Check for validation layers. If present, return True else False.
 	for (const char* layerName : validation::validationLayers) {
 		bool layerFound = false;
 
 		for (const auto& layerProperties : availableLayers) {
-			if (strcmp(layerName, layerProperties.layerName) == 0) {
+			if (std::strcmp(layerName, layerProperties.layerName) == 0) {
 				layerFound = true;
 				break;
 			}
@@ -37,3 +38,42 @@ bool HelloTriangleApplication::checkValidationSupport()
 	}
 	return true;
 }
+
+std::vector<const char*> HelloTriangleApplication::getRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtension;
+
+	// Get required instance extension from GLFW.
+	// Return Extension names and Extension Count.
+	glfwExtension = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	std::vector<const char*> extensions(glfwExtension, glfwExtension + glfwExtensionCount);
+
+	if (enableValidationLayers) {
+		/*
+		 * Note: We are using MACRO's. Here, VK_EXT_DEBUG_UTILS_EXTENSION_NAME is same
+		 * as VK_EXT_DEBUG_utils string. Macro avoids Typos.
+		 */
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+	return extensions;
+}
+
+void HelloTriangleApplication::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
+{
+	createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	createInfo.pfnUserCallback = static_cast<PFN_vkDebugUtilsMessengerCallbackEXT>(debugCallback);
+	createInfo.pUserData = nullptr; // Optional
+}
+
+VkBool32 HelloTriangleApplication::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData)
+{
+	std::cerr << "validation Layer: " << pCallbackData->pMessage << std::endl;
+	return VK_FALSE;
+}
+
