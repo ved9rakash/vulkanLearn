c3d6467519f2919efcf28c88a0ccea9f4c0d2d79
Ved9rakash <vs19990711@gmail.com>
2022-04-29 20:33:44 +0530

    triangle

--- include/helloTriangle.hpp
+++ include/helloTriangle.hpp
@@ -2,14 +2,12 @@
 // Created by Vaibhav on 23-04-2022.
 //
 
-#ifndef VULKANLEARN_INSTANCE_HPP
-#define VULKANLEARN_INSTANCE_HPP
+#ifndef VULKANLEARN_HELLOTRIANGLE_HPP
+#define VULKANLEARN_HELLOTRIANGLE_HPP
 
 #ifndef GLFW_INCLUDE_VULKAN
 	#define GLFW_INCLUDE_VULKAN
 #endif //GLFW_INCLUDE_VULKAN
-
-#include "validation.hpp"
 
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
@@ -17,6 +15,27 @@
 
 #include <stdexcept>
 #include <vector>
+#include <optional>
+
+//	Enable validation layers checking if(BUILD == DEBUG)
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif //NDEBUG
+
+//	Validation layers
+const std::vector<const char*> validationLayers = {
+		"VK_LAYER_KHRONOS_validation"
+};
+
+struct QueueFamilyIndices {
+	std::optional<uint32_t> graphicsFamily;
+
+	bool isComplete() {
+		return graphicsFamily.has_value();
+	}
+};
 
 
 class HelloTriangleApplication {
@@ -39,16 +58,31 @@ private:
 			);
 	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 	void setupDebugMessenger();
+	void pickPhysicalDevice();
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+	void createLogicDevice();
 
 private:
+	//	Window Initialization
 	static constexpr uint32_t WIDTH = 800;
 	static constexpr uint32_t HEIGHT = 600;
 	GLFWwindow* window;
+
+	//	Instance object
 	VkInstance instance;
+
+	// Debug messenger to manage debug callback.
 	VkDebugUtilsMessengerEXT debugMessenger;
+
+
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice device;
+	VkQueue graphicsQueue;
 };
 
 VkResult CreateDebugUtilsMessengerExt(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
 void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
 
-#endif //VULKANLEARN_INSTANCE_HPP
+
+#endif //VULKANLEARN_HELLOTRIANGLE_HPP
